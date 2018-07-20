854383355e854b5e3fe426a94e5e4c7ab3f32458
Alejandro Galaz <jgalaz@alioit.com>
2018-07-20 11:20:33 -0700

    two parameters

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 // Copyright 2018, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
+// Licensed under the Apache License, Version 2.0 (the 'License')
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,24 +11,59 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-'use strict';
+'use strict'
 
 // Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const {dialogflow, Table} = require('actions-on-google')
 
 // Import the firebase-functions package for deployment.
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
 // Instantiate the Dialogflow client.
-const app = dialogflow({debug: true});
+const app = dialogflow({debug: true})
+
+// Handle the Dialogflow intent named 'Default Welcome Intent'.
+app.intent('Default Welcome Intent', (conv) => {
+    conv.ask('here ready to help you master  👽' )
+})
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
 app.intent('favorite color', (conv, {color}) => {
-    const luckyNumber = color.length;
+    const luckyNumber = color.length
     // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber);
-});
+    conv.ask('Your lucky number is ' + luckyNumber)
+    conv.ask(new Table({
+        dividers: true,
+        columns: ['Key', 'Value'],
+        rows: [
+          ['color', color],
+        ]
+      }))       
+})
+
+// Handle the Dialogflow intent named 'Two Params'.
+// The intent collects the parameters named 'color , date'.
+app.intent('Two Params', (conv, {color, date}) => {
+    conv.ask('I am looking for the results')
+  
+        conv.ask('Here are the results : ')
+        conv.ask(new Table({
+            dividers: true,
+            columns: ['Key', 'Value'],
+            rows: [
+              ['color', color],
+              ['date', date]
+            ],
+          }))
+    
+                
+})
+
+
+
+
+
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
